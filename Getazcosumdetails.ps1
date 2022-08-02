d142ef4305b39008025ee7225b4cbb6925588228
tst1 <test1@gmail.com>
2022-08-02 07:00:05 +0530

    myfirst change

--- Getazcosumdetails.ps1
+++ Getazcosumdetails.ps1
@@ -1,7 +1,8 @@
-﻿Connect-AzAccount
+﻿
+Connect-AzAccount
 Get-AzSubscription
 Select-AzSubscription "QA - rbPulse"
 
 Get-AzConsumptionUsageDetail -StartDate 2022-04-01 -EndDate 2022-04-31 | Select-Object AccountName,AdditionalInfo,AdditionalProperties,BillableQuantity,BillingPeriodId,BillingPeriodName,ConsumedService,CostCenter,Currency,DepartmentName,Id,InstanceId,InstanceLocation,InstanceName,InvoiceId,InvoiceName,IsEstimated,MeterDetails,MeterId,Name,PretaxCost,Product,SubscriptionGuid,SubscriptionName,Tags,Type,UsageEnd,UsageQuantity,UsageStart `
 | ConvertTo-Csv -NoTypeInformation `
-| Set-Content C:\RAM\QA_2020_Final_April.csv
+| Set-Content C:\Badri\QA_2020_Final_April.csv
