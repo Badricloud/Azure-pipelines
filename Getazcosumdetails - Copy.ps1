cc44bb373322ace7744d9a0108874ce4bf6ebb6e
tst1 <test1@gmail.com>
2022-08-02 07:26:51 +0530

    second file

---
+++ Getazcosumdetails - Copy.ps1
@@ -0,0 +1,6 @@
+﻿
+Connect-AzAccount
+Get-AzSubscription
+Select-AzSubscription "test" 2022-04-01 -EndDate 2022-04-31 | Select-Object AccountName,AdditionalInfo,AdditionalProperties,BillableQuantity,BillingPeriodId,BillingPeriodName,ConsumedService,CostCenter,Currency,DepartmentName,Id,InstanceId,InstanceLocation,InstanceName,InvoiceId,InvoiceName,IsEstimated,MeterDetails,MeterId,Name,PretaxCost,Product,SubscriptionGuid,SubscriptionName,Tags,Type,UsageEnd,UsageQuantity,UsageStart `
+| ConvertTo-Csv -NoTypeInformation `
+| Set-Content C:\Badri\QA_2020_Final_April.csv
